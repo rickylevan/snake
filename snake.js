5cb49d4637dd5ea27f068d6698cb486377f919ba
Ricky LeVan <rick.levan@gmail.com>
2014-04-04 20:17:10 -0500

    basic framework for target location--white block--put in place

--- snake.js
+++ snake.js
@@ -8,48 +8,50 @@ var snakeColor = "#99cc44";
 var white = "#ffffff";
 var black = "#000000";
 
-var squareRadius = 13;
-var squaresInRow = 13;
-var squaresInColumn = 13;
-var n = 13;
+var squareRadius = 15;
+var columnLength = 13;
+var rowLength = 17;
 var squareSize = 35; // in pixels
-var gameWidth = squaresInRow * squareSize;
-var gameHeight = squaresInColumn * squareSize;
+var gameWidth = columnLength * squareSize;
+var gameHeight = rowLength * squareSize;
 
 // key global variable that will be modified 
 // by keystrokes, and acted upon after each interval
 // of gameSpeed
 var snakeNextDir = "right"; // arbitrary starting choice
 
-var gameSpeed = 70;
+var gameSpeed = 60;
 var snakeLength = 3; // starting value 
 
+// function to compute new location for target tile
+function assignNewTarget() {
+    randCol1 = Math.floor(Math.random()*rowLength);
+    randRow1 = Math.floor(Math.random()*columnLength);
+
+    // while the chosen location is inside the snake, no good!
+    // keep repeating until this is not the case
+    while (snakeState.containsList([randCol1,randRow1])) {
+        randCol1 = Math.floor(Math.random()*rowLength);
+        randRow1 = Math.floor(Math.random()*columnLength);
+    }
+
+    return [randCol1,randRow1];
+}
+
+
+// setting key canvas and context variables -- globals used often
 var canvas = document.getElementById("drawCanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = backgroundColor;
-ctx.fillRect(0, 0, gameWidth, gameHeight);
 
 function clearToBoardColor() {
     ctx.fillStyle = boardColor;
-    for (j = 0; j < n; j++) {
-        for (k = 0; k < n; k++) {
+    for (j = 0; j < rowLength; j++) {
+        for (k = 0; k < columnLength; k++) {
             roundRect(ctx, j * squareSize, k * squareSize, squareSize,
                       squareSize, squareRadius, true, true);
         }
     }
 }
-
-clearToBoardColor(); // initialize cleared board state
-var randCol = Math.floor(Math.random()*squaresInColumn);
-// assume snake is facing to the right. Make sure all snake originates on board
-var randRow = Math.floor(Math.random()*(squaresInRow-snakeLength)+snakeLength);
-randCol = 5;
-randRow = 5;
-var keyRectIndex = [randCol, randRow];
-// FIXME location currently independent of snake initial length
-//
-// assuming now that snake head is the LAST element of the list
-var snakeState = [[keyRectIndex[0]-2,keyRectIndex[1]],[keyRectIndex[0]-1,keyRectIndex[1]],[keyRectIndex[0],keyRectIndex[1]]];
 
 
 // ordering functions now in reverse order so nothing is used before it is defined
@@ -63,6 +65,15 @@ function paintSnake() {
     }
 }
 
+function paintTarget() {
+    ctx.fillStyle = white;
+    target = assignNewTarget();
+    roundRect(ctx, target[0] * squareSize, target[1] * squareSize,
+                  squareSize ,squareSize, squareRadius, true, true);
+}
+
+
+
 function drawNewGameBoard() {
 
     // first clear the board of everything:
@@ -70,31 +81,35 @@ function drawNewGameBoard() {
 
     // and now paint all the elements belonging to the snake:
     paintSnake();
+
+    // now we paint the target, *every single round*, but we have it change if 
+    // it is true that the snake is on the existing target
+    paintTarget();
 }
 
 function updateGameState() {
     // based on snakeNextDir, the resulting direction from user input, update
-    // the keyRectIndex, which indicates where the snake will move next
+    // the snakeHead, which indicates where the snake will move next
     switch(snakeNextDir) {
         case "left":
-            keyRectIndex[0] = (keyRectIndex[0] - 1).mod(squaresInRow);
+            snakeHead[0] = (snakeHead[0] - 1).mod(rowLength);
             break;
         case "up":
-            keyRectIndex[1] = (keyRectIndex[1] - 1).mod(squaresInColumn);
+            snakeHead[1] = (snakeHead[1] - 1).mod(columnLength);
             break;
         case "right":
-            keyRectIndex[0] = (keyRectIndex[0] + 1).mod(squaresInRow);
+            snakeHead[0] = (snakeHead[0] + 1).mod(rowLength);
             break;
         case "down":
-            keyRectIndex[1] = (keyRectIndex[1] + 1).mod(squaresInColumn);
+            snakeHead[1] = (snakeHead[1] + 1).mod(columnLength);
             break;
     }
     // now we mutate the snake based on this information
     littleArray = [];
-    littleArray[0] = keyRectIndex[0];
-    littleArray[1] = keyRectIndex[1];
-
+    littleArray[0] = snakeHead[0];
+    littleArray[1] = snakeHead[1];
     snakeState.push(littleArray);
+
     delete snakeState[0]; // will this stop endless memory expansion?
     snakeState.shift();
 
@@ -105,11 +120,6 @@ function updateGameState() {
 
 
 
-
-
-paintSnake();
-
-setInterval(updateGameState,gameSpeed);
 
 
 // update global variable in response to key stroke
@@ -132,4 +142,16 @@ $(document).keydown(function (e) {
 
 
 
+ctx.fillStyle = backgroundColor;
+ctx.fillRect(0, 0, gameWidth, gameHeight);
 
+clearToBoardColor(); // initialize cleared board state
+
+snakeHead = [5,5];
+snakeState = [[snakeHead[0]-2,snakeHead[1]],[snakeHead[0]-1,snakeHead[1]],[snakeHead[0],snakeHead[1]]];
+target = assignNewTarget();
+
+paintSnake();
+paintTarget();
+
+setInterval(updateGameState,gameSpeed);
--- utilities.js
+++ utilities.js
@@ -47,3 +47,29 @@ function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
   }        
 }
 
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a === null || b === null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+Array.prototype.containsList = function ( needle ) {
+   for (j = 0; j < this.length; j++) {
+       if (arraysEqual(this[j], needle)) {
+           return true;
+       }
+   }
+   return false;
+};
+
+
+
+
